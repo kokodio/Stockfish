a152af03dd8f68898fc481db357dbbd97a9923ac
Кирилл Зарипов <67618307+kokodio@users.noreply.github.com>
2025-05-22 12:43:04 +0500

    Adjust negative extension
    Bench: 1985912

--- src/search.cpp
+++ src/search.cpp
@@ -1180,7 +1180,7 @@ moves_loop:  // When in check, search starts here
 
             // If the ttMove is assumed to fail high over current beta
             else if (ttData.value >= beta)
-                extension = -3;
+                extension = ttCapture ? -2 : -3;
 
             // If we are on a cutNode but the ttMove is not assumed to fail high
             // over current beta
