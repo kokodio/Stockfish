9d5149764ebf409f9465d3a890e53221bd58e272
Кирилл Зарипов <67618307+kokodio@users.noreply.github.com>
2025-05-25 18:55:34 +0500

    razoring cutNode
    Bench: 2514286

--- src/search.cpp
+++ src/search.cpp
@@ -843,9 +843,16 @@ Value Search::Worker::search(
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
     // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && eval < alpha - 486 - 325 * depth * depth)
-        return qsearch<NonPV>(pos, ss, alpha, beta);
+    if (!PvNode)
+    {
+        Value razoringMargin = 486 + 325 * depth * depth;
 
+        if (cutNode && (ss-1)->cutoffCnt > 2)
+            razoringMargin += 50 + 25 * depth;
+            
+        if (eval < alpha - razoringMargin)
+            return qsearch<NonPV>(pos, ss, alpha, beta);
+    }
     // Step 8. Futility pruning: child node
     // The depth condition is important for mate finding.
     {
