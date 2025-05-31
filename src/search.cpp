5daa83eac931f05fc37c81318ca588921bda28ba
Кирилл Зарипов <67618307+kokodio@users.noreply.github.com>
2025-05-31 23:33:56 +0500

    v2
    bench: 2732114

--- src/search.cpp
+++ src/search.cpp
@@ -1396,7 +1396,7 @@ moves_loop:  // When in check, search starts here
                 else
                 {
                     // Reduce other moves if we have found at least one score improvement
-                    if (depth > 2 && depth < 16 && !is_decisive(value))
+                    if (depth > 2 && depth < 16 && !is_decisive(value) && !improving)
                         depth -= 2;
 
                     assert(depth > 0);
