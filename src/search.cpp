5a5790a4b2e9848eb805e43f05299cb469b293b4
Кирилл Зарипов <67618307+kokodio@users.noreply.github.com>
2025-05-31 19:18:39 +0500

    Adj by improving
    bench: 2131314

--- src/search.cpp
+++ src/search.cpp
@@ -1389,7 +1389,7 @@ moves_loop:  // When in check, search starts here
                 if (value >= beta)
                 {
                     // (* Scaler) Especially if they make cutoffCnt increment more often.
-                    ss->cutoffCnt += (extension < 2) || PvNode;
+                    ss->cutoffCnt += (extension < 2) || PvNode || improving;
                     assert(value >= beta);  // Fail high
                     break;
                 }
