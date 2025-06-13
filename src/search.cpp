f71205672ef17b1ebd6e04d876e3b6a13f67b21e
Кирилл Зарипов <67618307+kokodio@users.noreply.github.com>
2025-06-13 11:10:45 +0500

    take 4
    bench: 2151108

--- src/search.cpp
+++ src/search.cpp
@@ -1227,7 +1227,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease/increase reduction for moves with a good/bad history
         r -= ss->statScore * 826 / 8192;
-        r -= ss->lmrMissCount * 256;
+        r -= ss->lmrMissCount * 512;
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
